ab71a4a306a8d17fb53c9099575454c144ace3fc
Alex Shearer <alex.shearer23@gmail.com>
2019-11-25 10:11:56 -0600

    Added in gitignore to ignore node modules, installed packages for server

--- backend/secret.js
+++
@@ -1,8 +0,0 @@
-const secrets = {
-    //The URL that we use to connect to the MongoDB Atlas Cluster
-    dbUrl: 'mongodb+srv://alexshearer:123password@cluster0-ahthe.mongodb.net/test?retryWrites=true&w=majority'
-};
-
-const getSecret = key => secrets[key];
-
-module.exports = getSecret;
